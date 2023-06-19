95997170da0b0bad4dc4b6395112ce2f053e470c
str26 <estherj_94@hotmail.com>
2023-06-18 23:58:02 -0500

    se agrego boton para imprimir y descargar en pdf

---
+++ portafolio.js
@@ -0,0 +1,20 @@
+
+const printBtn = document.getElementById("print");
+
+printBtn.addEventListener("click", function() {
+    print();
+})
+
+
+
+
+
+function generarPdf(){
+
+const doc = new jsPDF();
+let genPdf = document.getElementById("portafolio").value;
+doc.text(genPdf, 10, 10);
+doc.save("devNurse_portafolio.pdf");
+
+
+}
